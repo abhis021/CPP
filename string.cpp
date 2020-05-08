21af26b6e5c12a3ef22cd8e01e15601e6098ff90
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2020-05-08 11:04:50 +0530

    string
    string operation

---
+++ string.cpp
@@ -0,0 +1,22 @@
+//Code to input and print string
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Code by abhis021@github
+        string a, b;
+        cin >> a >> b;
+
+        cout << a.length() << ' ' << b.length() << endl;
+
+        cout << a + b << endl;
+
+        swap(a[0], b[0]);
+        cout << a << ' ' << b << endl;
+
+
+        return 0;
+}
+
