68415052edf46a5432aee7544d47fd951f49c1ac
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2021-03-24 18:23:04 +0530

    aho

--- triangle.cpp
+++ triangle.cpp
@@ -1,28 +1,16 @@
-//code by abhis021@github
 #include<iostream>
 using namespace std;
 int main()
 {
-	int side1, side2, side3,testcasenum, match=0;
-	cout<<"Enter testcase number: ";
-    cin>>testcasenum;
-	cout<<"Enter length of side1"<<endl;
-
-	cin>>side1;
-
-	cout<<"Enter length of side2"<<endl;
-
-	cin>>side2;
-
-	cout<<"Enter length of side3"<<endl;
-
-	cin>>side3;
-
-	printf("Test case # %d", testcasenum);
-
+    int side1, side2, side3, match=0;
+    cout<<"Enter length of side1"<<endl;
+    cin>>side1;
+    cout<<"Enter length of side2"<<endl;
+    cin>>side2;
+    cout<<"Enter length of side3"<<endl;
+    cin>>side3;
     if(side1==side2)
         match=match+1;
-
     else if(side1==side3)
         match=match+2;
     else if(side2==side3)
@@ -30,30 +18,30 @@ int main()
     if(match==0)
     {
         if((side1+side2)<=side3)
-            cout<<"\n not a triangle";
+            cout<<"not a triangle";
         else
         {
             if((side1+side3)<=side2)
-                cout<<"\n not a triangle";
+                cout<<"not a triangle";
         else
-            cout<<"\n triangle is scalene";
+            cout<<"triangle is scalene";
 
         }
     }
         else if(match==2)
-	{
-		if((side1+side3)<=side2)
-			cout<<"\n not a triangle";
-		else
-			cout<<"\n triangle is isosceles";
-	}
+    {
+        if((side1+side3)<=side2)
+            cout<<"not a triangle";
+        else
+            cout<<"triangle is isosceles";
+    }
     else if(match==3)
-	{
-		if((side2+side3)<=side1)
-			cout<<"\n not a triangle";
-		else
-			cout<<"\n triangle is isoscleles";
-			}
-		else
-			cout<<"\n triangle is equilateral";
+    {
+        if((side2+side3)<=side1)
+            cout<<"not a triangle";
+        else
+            cout<<"triangle is isoscleles";
+            }
+        else
+            cout<<"triangle is equilateral";
     }
